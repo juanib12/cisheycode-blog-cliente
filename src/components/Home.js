364704c880e4746f82db0ac0525395bcf02bad66
juanib12 <bjuan.ignacio11@gmail.com>
2022-10-26 19:36:42 -0300

    add loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const options = {
@@ -16,45 +17,52 @@ const Home = () => {
     axios
       .request(options)
       .then((response) => {
+        setLoading(true);
         setData(response.data[response.data.length - 1]);
       })
       .catch((err) => {
         console.log(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
   return (
     <section className="home bd-grid">
-      <h2>Último artículo</h2>
-      {data ? (
-        <Link to={`/articulo/${data._id}`}>
-          <div className="last_post" key={data._id}>
-            <div className="last_post_img">
-              <img src={data.image} width="650" />
-            </div>
-            <div className="last_post_info">
-              <div className="last_post_date">
-                <p>{data.createtime}</p>
-              </div>
-              <div className="last_post_title">
-                <h3>{data.title}</h3>
-              </div>
-              <div className="last_post_description">
-                <p>{data.resume}</p>
-              </div>
-              <div className="last_post_footer">
-                <img
-                  src={data.avatar}
-                  width="50"
-                  style={{ borderRadius: "50%" }}
-                />
-                <p>{data.user}</p>
-              </div>
-            </div>
-          </div>
-        </Link>
+      {loading == true ? (
+        <p>LOADINGGGGGGGGGGGGGG</p>
       ) : (
-        <h3>Ningún articulo para mostrar.</h3>
+        <>
+          <h2>Último artículo</h2>
+
+          <Link to={`/articulo/${data._id}`}>
+            <div className="last_post" key={data._id}>
+              <div className="last_post_img">
+                <img src={data.image} width="650" />
+              </div>
+              <div className="last_post_info">
+                <div className="last_post_date">
+                  <p>{data.createtime}</p>
+                </div>
+                <div className="last_post_title">
+                  <h3>{data.title}</h3>
+                </div>
+                <div className="last_post_description">
+                  <p>{data.resume}</p>
+                </div>
+                <div className="last_post_footer">
+                  <img
+                    src={data.avatar}
+                    width="50"
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <p>{data.user}</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </>
       )}
 
       <Recomendados />
