328ca9473fdcde76387d2c5573f62022961248bc
juanib12 <bjuan.ignacio11@gmail.com>
2022-10-27 15:42:32 -0300

    f

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,42 @@
 const Footer = () => {
   return (
-    <section className="bd-grid">
-      <div className="footer">
-        <div className="footer_items">
-          <i class="bx bxl-gmail"></i>
-          <a href="#" className="footer_links">
-            Contactame
-          </a>
+      <section className="bd-grid">
+        <div className="footer">
+          <div className="footer_items">
+            <i class="bx bxl-gmail"></i>
+            <a href="#" className="footer_links">
+              Contactame
+            </a>
+          </div>
+          <div className="footer_items">
+            <i class="bx bxl-twitter"></i>
+            <a href="#" className="footer_links">
+              Twitter
+            </a>
+          </div>
+          <div className="footer_items">
+            <i class="bx bxl-instagram-alt"></i>
+            <a href="#" className="footer_links">
+              Instragram
+            </a>
+          </div>
+          <div className="footer_items">
+            <i class="bx bxl-github"></i>
+            <a href="#" className="footer_links">
+              GitHub
+            </a>
+          </div>
+          <div className="footer_items">
+            <i class="bx bxl-linkedin-square"></i>
+            <a href="#" className="footer_links">
+              Linkedin
+            </a>
+          </div>
+          <div className="footer_name">
+            <p>Copyright © 2022 - Juan Ignacio Bianco</p>
+          </div>
         </div>
-        <div className="footer_items">
-          <i class="bx bxl-twitter"></i>
-          <a href="#" className="footer_links">
-            Twitter
-          </a>
-        </div>
-        <div className="footer_items">
-          <i class="bx bxl-instagram-alt"></i>
-          <a href="#" className="footer_links">
-            Instragram
-          </a>
-        </div>
-        <div className="footer_items">
-          <i class="bx bxl-github"></i>
-          <a href="#" className="footer_links">
-            GitHub
-          </a>
-        </div>
-        <div className="footer_items">
-          <i class="bx bxl-linkedin-square"></i>
-          <a href="#" className="footer_links">
-            Linkedin
-          </a>
-        </div>
-        <div className="footer_name">
-          <p>Copyright © 2022 - Juan Ignacio Bianco</p>
-        </div>
-      </div>
-    </section>
+      </section>
   );
 };
 
